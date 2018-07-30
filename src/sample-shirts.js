8a79fe7bd8349db201a31dc074f6d3277aae85e3
techesuraj <suraj.gantedi@gmail.com>
2018-07-29 23:16:59 -0700

    Added Sample data

---
+++ src/sample-shirts.js
@@ -0,0 +1,43 @@
+const shirts = {
+  shirt1:{
+    name:'Cheetah black',
+    image:'./assets/images/shirt1.jpg',
+    wore:false,
+  },
+  shirt2: {
+    name:'light Pink',
+    image:'./assets/images/shirt2.jpeg',
+    wore:false
+  },
+  shirt3: {
+    name:'Flower Flowers',
+    image:'./assets/images/shirt3.jpeg',
+    wore:false
+  },
+  shirt4: {
+    name:'Tress Chitla',
+    image:'./assets/images/shirt4.jpeg',
+    wore:false
+  },
+  shirt5: {
+    name:'Pink Strips',
+    image:'./assets/images/shirt5.jpeg',
+    wore:false
+  },
+  shirt6: {
+    name:'Light Crimson',
+    image:'./assets/images/shirt6.jpeg',
+    wore:false
+  },
+  shirt7: {
+    name:'Navy Blue',
+    image:'./assets/images/shirt7.jpeg',
+    wore:false
+  },
+  shirt8: {
+    name:'Grey Strips',
+    image:'./assets/images/shirt8.jpeg',
+    wore:false
+  }
+}
+export default shirts;
